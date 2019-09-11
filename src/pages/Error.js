97c5917faa536b1ef4930d31ffee8b3f7bd68b46
Bart <adv1key@o2.pl>
2019-09-11 23:55:38 +0200

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -5,17 +5,75 @@ const StyledError = styled.section`
 	width: 50vw;
 	background: #ececf0;
 
-	h1 {
+	.container {
 		padding: 40px;
+	}
+
+	h1 {
 		font-size: 1.625rem;
 		font-weight: 600;
+	}
+
+	.flex {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		margin-top: 2rem;
+		text-align: center;
+
+		p:nth-of-type(2) {
+			margin-top: 1rem;
+		}
+
+		a {
+			text-decoration: none;
+			background: -webkit-linear-gradient(top, #fe843f, #fc5a37);
+			-webkit-background-clip: text;
+			background-clip: text;
+			-webkit-text-fill-color: transparent;
+			font-weight: bold;
+		}
+	}
+
+	@media (max-width: 1359px) {
+		padding: 12.5px;
+
+		.container {
+			padding: 20px;
+		}
+	}
+
+	@media (max-width: 1124px) {
+		width: 80%;
+	}
+
+	@media (max-width: 813px) {
+		width: 100%;
+
+		.container {
+			padding: 10px;
+		}
 	}
 `;
 
 const Error = () => {
 	return (
 		<StyledError>
-			<h1>404 not found</h1>
+			<div className="container">
+				<h1>404 not found</h1>
+				<div class="flex">
+					<p>
+						If you found this page, it can mean that it is not yet supported or
+						you've entered the invalid URL. Please keep in mind that this is
+						still not completed project.
+					</p>
+					<p>
+						If you have any questions, simply contact with me{' '}
+						<a href="mailto:me@bartzalewski.com">here</a>.
+					</p>
+				</div>
+			</div>
 		</StyledError>
 	);
 };
